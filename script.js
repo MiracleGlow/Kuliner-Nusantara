3936b3dcbe683ae09299b7737ed2ab91433e8e93
Primaa <muktiprima111@gmail.com>
2024-08-10 05:07:57 +0700

    update
    Signed-off-by: Primaa <muktiprima111@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-function toggleMenu() {
-    const nav = document.getElementById("navMenu");
-    const hamburger = document.querySelector(".Menu-btn");
-    nav.classList.toggle("nav-open");
-    hamburger.classList.toggle("Menu-btn-open");
-    
-    // Add animation delay for each menu item
-    const navItems = document.querySelectorAll(".nav-list li");
-    navItems.forEach((item, index) => {
-        item.style.animationDelay = `${index * 0.1}s`;
-    });
-  }
+const searchInput = document.getElementById('searchInput');
+const searchResults = document.getElementById('searchResults');
+
+const foods = [
+    { name: 'Rendang', url: 'rendang.html' },
+    { name: 'Empek-empek Palembang', url: 'empek2.html' },
+    { name: 'Rujak Cingur', url: 'rujakcingur.html' },
+    { name: 'Soto Banjar', url: 'sotobanjar.html' },
+    { name: 'Dawet', url: 'Dawet.html' },
+    { name: 'Wedang Ronde', url: 'WedangRonde.html' }
+];
+
+searchInput.addEventListener('input', function() {
+    const query = searchInput.value.toLowerCase();
+    searchResults.innerHTML = '';
+    if (query) {
+        const filteredFoods = foods.filter(food => food.name.toLowerCase().includes(query));
+        if (filteredFoods.length > 0) {
+            searchResults.style.display = 'block';
+            filteredFoods.forEach(food => {
+                const link = document.createElement('a');
+                link.href = food.url;
+                link.textContent = food.name;
+                searchResults.appendChild(link);
+            });
+        } else {
+            searchResults.style.display = 'none';
+        }
+    } else {
+        searchResults.style.display = 'none';
+    }
+});
